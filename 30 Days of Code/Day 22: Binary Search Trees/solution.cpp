00fb302e9f565985580c646368d465556fed683d
Shuxiao <dingshuxiao@126.com>
2020-02-12 14:06:38 +0100

    0212

---
+++ 30 Days of Code/Day 22: Binary Search Trees/solution.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+		int getHeight(Node* root){
+            //Write your code here    
+            int left_height, right_height;
+            if (root->left == NULL) {
+                left_height = 0;
+            } else {
+                left_height = getHeight(root->left) + 1;
+            }
+            if (root->right == NULL) {
+                right_height = 0;
+            } else {
+                right_height = getHeight(root->right) + 1;
+            }
+            return max(left_height, right_height);
+        }
+
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+    return 0;
+}
